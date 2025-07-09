a1e89c9e3e86509abffe92b7cbbc63a9e383f1aa
produtik <admin@produtik.com>
2025-07-09 01:37:37 -0300

    fx

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 }));
